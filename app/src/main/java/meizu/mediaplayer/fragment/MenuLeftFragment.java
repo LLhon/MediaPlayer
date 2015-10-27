0a9eda416ffdc0ead4c204d9d31a9a993dff9311
liuhehong <honhege@163.com>
2015-10-27 11:27:26 +0800

    show menu

---
+++ app/src/main/java/meizu/mediaplayer/fragment/MenuLeftFragment.java
@@ -0,0 +1,21 @@
+package meizu.mediaplayer.fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2015/10/26.by {ZM}
+ */
+public class MenuLeftFragment extends BaseFragment {
+
+    @Override
+    protected View initView(LayoutInflater inflater, ViewGroup container) {
+        return null;
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+    }
+}
